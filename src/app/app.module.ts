5375689d7cdaf15e51e25f34d34872870549da10
tirupatc <tirupatc@oregonstate.edu>
2018-07-15 16:25:48 -0700

    Assignment-2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { DishService } from './services/dish.service';
+import { LeaderService } from './services/leader.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
@@ -42,7 +43,7 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
       AppRoutingModule,
       MatButtonModule
   ],
-  providers: [DishService],
+  providers: [DishService, LeaderService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
